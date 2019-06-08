3a7d11d1e053f24287ec498d3ce6e5fa799894bf
Winta Hailai <wintahailai@Wintas-MacBook-Pro.local>
2019-06-07 19:53:38 -0700

    new code from user2

--- test1.py
+++ test1.py
@@ -1,3 +1,4 @@
 print ("this is test2")
 print (" adding to test2")
-print (" add new code from user1 after user2 clone/pull") 
+print (" add new code from user1 after user2 clone/pull")
+print (" add new code from user2 ") 
